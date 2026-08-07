1581e8cfeba01ffc4f57510e07a06df6b8aee6f6
Dev/Null <triple-xl-paladin@users.noreply.github.com>
2026-08-07 08:02:52 +0100

    - Inplemented traits - Cleaned up project folder and removed personal and test files

--- test.js
+++
@@ -1,8 +0,0 @@
-// This program prints 'Hello, World!' to the console.
-function helloWorld() {
-  // Print 'Hello, World!'
-  console.log('Hello, World!');
-}
-
-// Run the function to print 'Hello, World!' to the console.
-helloWorld();
